c4969bb2f6b75d448964b22b1855848b8e98c252
markus@clardy.eu <markus@clardy.eu>
2017-12-01 20:35:38 +0000

    next

--- routes/root.js
+++ routes/root.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.render('index.html', {
+	res.render('first_view', {
 		title: "Root",
 		url: "https://clardy.eu/markus"
    	});
