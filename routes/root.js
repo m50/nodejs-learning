a3b758b86d8210e71be30d63a3384bfdfa4d7a83
Marisa <markus@clardy.eu>
2017-12-01 20:17:30 +0000

    next

--- routes/root.js
+++ routes/root.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-   res.render('first_view');
+	res.render('first_view', {
+		title: "Root",
+		url: "https://clardy.eu/markus"
+   	});
 });
 router.post('/', function(req, res){
-   res.render('first_view');
+   	res.render('first_view');
 });
 
 //export this router to use in our index.js
