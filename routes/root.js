b0a38c300ae2e04b2d00fd69728422c11c154722
markus@clardy.eu <markus@clardy.eu>
2017-12-01 20:43:07 +0000

    next

--- routes/root.js
+++ routes/root.js
@@ -1,10 +1,21 @@
-var express = require('express');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 var router = express.Router();
 
+router.use(cookieParser());
+router.use(session({secret: "Shh! It's a secret!"}));
+
 router.get('/', function(req, res){
+	if(req.session.page_views) {
+		req.session.page_views++;
+	} else {
+		req.session.page_views = 1;
+	}
 	res.render('first_view', {
 		title: "Root",
-		url: "https://clardy.eu/markus"
+		url: "https://clardy.eu/markus",
+		visits: req.session.page_views
    	});
 });
 router.post('/', function(req, res){
