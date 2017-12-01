6fc0c5ee7dac2c37282e523ace4c9e09b31c127b
markus@clardy.eu <markus@clardy.eu>
2017-12-01 20:33:44 +0000

    next

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
+const engines = require('consolidate');
 var app = express();
 
 // app.use(express.static('views'));
 // app.use(express.static('static'));
 
-app.set('view engine', 'pug');
-app.set('views','./views');
+// app.set('view engine', 'pug');
+// app.set('views','./views');
+
+app.set('views', './views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
 
 var rootjs = require('./routes/root.js');
 var testjs = require('./routes/test.js');
