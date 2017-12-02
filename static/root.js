410af918962e2dcf56ca6cce0f743d8ee6267c27
markus@clardy.eu <markus@clardy.eu>
2017-12-02 23:55:36 +0000

    next

--- static/root.js
+++ static/root.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
 
 $('p').on('click', function () {
 	console.log($(this).text);
-	$(this).text("JQuery changed this!");
+	$(this).text("JQuery changed this! " + $(this).text());
 	console.log('JQuery on click');
 });
 
