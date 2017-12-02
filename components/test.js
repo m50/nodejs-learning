f084f02155bd2b6f4ce0b47436dffe7e6ba0712a
markus@clardy.eu <markus@clardy.eu>
2017-12-02 23:16:27 +0000

    next

--- components/test.js
+++ components/test.js
@@ -3,7 +3,7 @@ var express = require('express');
 var component = {}
 
 component.page404 = (req, res) => {
-	res.sendFile('/var/www/nodejs/views/404.html');
+	res.sendFile('../views/404.html');
 }
 
 module.exports = component;
