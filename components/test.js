e85a58b90cf1ab55b0d21caed88c7182895f748f
markus@clardy.eu <markus@clardy.eu>
2017-12-02 23:23:52 +0000

    next

--- components/test.js
+++ components/test.js
@@ -4,7 +4,7 @@ const path = require('path');
 var component = {}
 
 component.page404 = (req, res) => {
-	res.sendFile(path.resolve('../views/404.html'));
+	res.sendFile(path.resolve('./views/404.html'));
 }
 
 module.exports = component;
