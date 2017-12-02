bc0e865dc92936d3e8053bf01d9d954a5eafaa06
markus@clardy.eu <markus@clardy.eu>
2017-12-02 23:26:57 +0000

    next

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-var rootjs = require('./routes/root.js');
-var testjs = require('./routes/test.js');
-var formjs = require('./routes/form.js');
+const rootjs = require('./routes/root.js');
+const testjs = require('./routes/test.js');
+const formjs = require('./routes/form.js');
+const moviesjs = require('./routes/movies.js');
 
 app.use('/', rootjs);
 app.use('/test', testjs);
 app.use('/form', formjs);
+app.use('/moves', moviesjs);
 
 app.listen(6080, '127.0.0.1');
