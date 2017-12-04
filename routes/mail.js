6d7e0330ce361dcbaf6d5cf54b87a48c6bc51bd3
markus@clardy.eu <markus@clardy.eu>
2017-12-04 22:06:01 +0000

    next

--- routes/mail.js
+++ routes/mail.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/mail';
-const client = new pg.Client(connectionString);
 var upload = multer();
 var router = express.Router();
 
@@ -12,24 +11,24 @@ router.get('/mail', (req, res, next) => {
 });
 
 router.get('/mail/policies', (req, res, next) => {
-	// pg.connect(connectionString, (err, client, done) => {
-	// 	var results = [];
-	// 	if(err) {
-	// 		done();
-	// 		console.log(err);
-	// 		return res.status(500).json({success: false, data: err});
-	// 	}
-	// 	const q = "SELECT * FROM policy;";
-	// 	const query = client.query(q);
-	// 	query.on('row', (row) => {
-	// 		results.push(row);
-	// 	});
+	pg.connect(connectionString, (err, client, done) => {
+		var results = [];
+		if(err) {
+			done();
+			console.log(err);
+			return res.status(500).json({success: false, data: err});
+		}
+		const q = "SELECT * FROM policy;";
+		const query = client.query(q);
+		query.on('row', (row) => {
+			results.push(row);
+		});
 
-	// 	query.on('end', () => {
-	// 		done();
-	// 		return res.json(results);
-	// 	});
-	// });
+		query.on('end', () => {
+			done();
+			return res.json(results);
+		});
+	});
 });
 
 module.exports = router;
