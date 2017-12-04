7e5e35795fbea2f1e6c9a21acf8606cc50b7e394
Markus <markus@markus.copperf.local>
2017-12-04 17:19:34 +0000

    next

--- components/urlshortener.js
+++ components/urlshortener.js
@@ -17,13 +17,11 @@ function makeid() {
 component.createShortener = (uri) => {
 	const key = makeid();
 	client.set("urishort:"+key, uri);
-	// console.log('Key: '+key+"; URI: "+uri);
 	return key;
 };
 
 component.getURI = (key, res) => {
 	client.get("urishort:"+key, (err, reply) => {
-		// console.log("getURI: Key="+key+"; URL="+reply);
 		res.redirect(reply);
 	});
 };
