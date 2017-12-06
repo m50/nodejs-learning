0a00769ce5068d7c7dc2b367630ef44563a04b92
Markus <markus@markus.copperf.local>
2017-12-06 12:37:42 +0000

    next

--- gulp.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const react = require('gulp-react');
-
-gulp.task("default", () => {
-	gulp.src( "./static/jsx/*.jsx" ).pipe( babel({ plugins: ['transform-react-jsx'] }) ).pipe( gulp.dest("./static/js/react/") );
-	console.log('gulp');
-});
