e5ce54648d1cdc9f687ad445110d0a1b956acdf6
Markus <markus@markus.copperf.local>
2017-12-07 14:41:25 +0000

    next

--- static/jsx/Navbar.jsx
+++ static/jsx/Navbar.jsx
@@ -7,13 +7,14 @@ class Navbar extends React.Component {
 	}
 
 	onClick (e) {
+		console.log(e.target.value);
 		this.props.onclick(e.target.value);
 	}
 
 	render () {
 		const navbar = (
 			<ul className='navbar'>
-				<li className='navitem' value='main' onClick={this.onClick}>Main</li>
+				<li className='navitem current' value='main' onClick={this.onClick}>Main</li>
 				<li className='navitem' value='page2' onClick={this.onClick}>Page 2</li>
 			</ul>);
 		return navbar;
