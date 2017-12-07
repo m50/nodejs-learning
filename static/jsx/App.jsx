a1f8272a347ac647e9a3460edb2b5e5b4532109a
Markus <markus@markus.copperf.local>
2017-12-07 12:07:52 +0000

    next

--- static/jsx/App.jsx
+++ static/jsx/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
-export class App extends React.Component {
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!</p>
+         </div>
+      );
+   }
+}
+
+class App extends React.Component {
 	render () {
 		return (<div id='body'>
 					<p>This is a another test!</p>
 					<p>Just updating</p>
 					<p>This is a test of seperating my jsx files</p>
+					<Content />
 				</div>);
 	}
 }
+
+
+export default App;
