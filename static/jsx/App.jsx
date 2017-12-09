1db25521e5404c428aeadd3edeba1fd1363a9478
markus@clardy.eu <markus@clardy.eu>
2017-12-09 12:17:59 +0000

    build process

--- static/jsx/App.jsx
+++ static/jsx/App.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
-import Content from './Content.jsx'
+import Navbar from './Navbar.jsx';
+import Navitem from './Navitem.jsx';
 
 class App extends React.Component {
-	render () {
-		const App = (
-			<div>
-				<Content />
-			</div>
-			);
-		return App;
+	constructor (props) {
+		super(props);
+		this.state = {page: 'main'};
+
+		this.onNavbar = this.onNavbar.bind(this);
+	}
+
+	onNavbar(page) {
+		this.setState({ page: page });
+	}
+
+	render() {
+		var content = <div></div>;
+
+		if(this.state.page=='main') {
+			content = (
+				<div>
+					<h2>Main</h2>
+					<p>The content text!!!</p>
+					<p>This is a another test!</p>
+					<p>Just updating</p>
+					<p>This is a test of seperating my jsx files</p>
+				</div>
+				);
+		} else if(this.state.page=='page2') {
+			content = (
+				<div>
+					<h2>Second Page</h2>
+					<p>Other content text!!!</p>
+				</div>
+				);
+		}
+		var body = (<div id='body'>
+						<Navbar page={this.state.page}>
+							<Navitem curPage={this.props.page} onclick={this.onNavbar} page='main' pageTitle='Main' />
+							<Navitem curPage={this.props.page} onclick={this.onNavbar} page='page2' pageTitle='Page 2' />
+						</Navbar>
+						{content}
+					</div>);
+
+		return body;
 	}
 }
 
