9801305dd70f44b06951c7a81943fae86b3548ed
markus@clardy.eu <markus@clardy.eu>
2017-12-11 20:14:43 +0000

    next

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const { Client } = require('pg');
 const path = require('path');
+const client = new Client({
+	user: 'mailu',
+	host: '127.0.0.1',
+	port: 5432,
+	database: 'blog',
+	password: 'mailu-passwd'
+});
 var router = express.Router();
 
 router.get('/', (req, res) => {
@@ -19,11 +27,29 @@ router.get('/:id(\d+)', (req, res) => {
 });
 
 router.get('/posts', (req, res) => {
-	res.json
+	client.connect();
+	client.query('SELECT id, time_written AS date, text AS post FROM posts', (err, res) => {
+		if(err) {
+			res.status(404);
+			res.json({ status: "Failure", error: err });
+		} else {
+			res.json({ status: "Success", posts: res });
+		}
+		cliend.end();
+	});
 });
 
 router.get('/posts/:id(\d+)', (req, res) => {
-
+	client.connect();
+	client.query('SELECT id, time_written AS date, text AS post FROM posts WHERE id = $1::INT', [ req.params.id ], (err, res) => {
+		if(err) {
+			res.status(404);
+			res.json({ status: "Failure", error: err });
+		} else {
+			res.json({ status: "Success", posts: res });
+		}
+		cliend.end();
+	});
 });
 
 router.get('/wiki', (req, res) => {
