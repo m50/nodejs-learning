e88b82a0ff9ae3755e51e5bd42f23c91143da582
Marisa <marisa@clardy.eu>
2017-12-12 09:47:47 +0000

    next

--- static/jsx/Posts.jsx
+++ static/jsx/Posts.jsx
@@ -47,9 +47,9 @@ class Posts extends React.Component {
 				</div>
 				);
 		}
+				// <h2 onClick={ () => { this.headerClick(-1); } }>Blog</h2>
 		return (
 			<div id='Blog'>
-				<h2 onClick={ () => { this.headerClick(-1); } }>Blog</h2>
 				{this.state.posts}
 				{lastSection}
 			</div>
