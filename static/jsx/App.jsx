32b8793ab996bab89229f3823ac9f7305fa01354
Marisa <marisa@clardy.eu>
2018-01-12 11:31:02 +0000

    SPA my React page

--- static/jsx/App.jsx
+++ static/jsx/App.jsx
@@ -7,7 +7,8 @@ import Wiki from './Wiki.jsx';
 class App extends React.Component {
 	constructor (props) {
 		super(props);
-		this.state = {page: window.location.pathname.match(/blog\/([^\/])/)[0].substring(6)};
+		console.log(window.location.pathname.match(/blog\/([^\/])/)[0];
+		this.state = {page: window.location.pathname.match(/blog\/([^\/])/)[0]};
 
 		this.onNavbar = this.onNavbar.bind(this);
 	}
