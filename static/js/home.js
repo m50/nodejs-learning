4b216ec2736f93899253bcb4c8a4db5088cadb8b
Marisa <marisa@clardy.eu>
2018-01-15 11:05:28 +0000

    updating Angular Code

--- static/js/home.js
+++ static/js/home.js
@@ -9,10 +9,21 @@ app.component('informationSection', {
 
 app.controller('MainController', ['$scope', ($scope) => {
 	$scope.sections = [
-					{title: 'Home Page', content: 'I am utilizing AngularJS 1.6.'},
-					{title: 'CV', content: 'First page I ever utilized JQuery.'},
-					{title: 'URL Shortener', content: 'Utilizing Redis and implemented Forms using NodeJS. It was my first NodeJS web-app.'},
-					{title: 'Blog', content: 'Utilizing React for the front-end, and a RESTful API for the backend, the blog stores data in a PostgreSQL Database, and was my second web-app.'},
-					{title: 'Hidden Pages', content: 'There are hidden pages as well, that I utilized when following tutorials to learn new things, but they aren\'t necessarily things I made.'}
-				];
+		{
+			title: 'Home Page', 
+			content: 'I am utilizing AngularJS 1.6. Each of these entries are in a Javascript Array, and are rendered out using an html template'
+		},
+		{
+			title: 'CV', 
+			content: 'First page I ever utilized JQuery on.'
+		},
+		{
+			title: 'URL Shortener', 
+			content: 'Utilizing Redis for the database, it was my first NodeJS web-app.'
+		},
+		{
+			title: 'Blog', 
+			content: 'Utilizing React for the front-end, and a RESTful API for the backend, the blog stores data in a PostgreSQL Database, and was my second web-app.'
+		}
+	];
 }]);
