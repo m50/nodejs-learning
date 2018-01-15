491c68d3ff5014e4577a975a3d25336a9541bbfe
Marisa <marisa@clardy.eu>
2018-01-15 15:33:37 +0000

    blog <base> tag

--- static/jsx/Posts.jsx
+++ static/jsx/Posts.jsx
@@ -70,7 +70,6 @@ class Posts extends React.Component {
 			if(isNaN(prevPage) || prevPage == 0) {
 				prevPage = '';
 			}
-			console.log(this.state.maxid);
 			if(isNaN(nextPage) || nextPage > this.state.maxid) {
 				nextPage = '';
 			}
